781aeedfe2ca1095917e754f0f8846a959a74f60
Igor Perekalskiy <perekalskiy_igor@mail.ru>
2022-12-29 10:34:34 +0500

    os

---
+++ system.py
@@ -0,0 +1,6 @@
+import os
+os.system('colorcpl.exe')
+
+# https://py3dev.ru/modules/os/#ospath
+# http://allmbs.ru/cmdsys-01.html
+# https://www.k-max.name/windows/zapusk-administrativnyx-instrumentov-iz-cmd/
