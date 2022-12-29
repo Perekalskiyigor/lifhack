5e354573d2384b0c52d595cab108ed2a75c63416
Igor Perekalskiy <perekalskiy_igor@mail.ru>
2022-12-29 17:18:59 +0500

    insert output

--- email.py
+++ email.py
@@ -3,5 +3,5 @@ import smtplib
 smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
 smtpObj.starttls()
 smtpObj.login('perekalskiy_igor@mail.ru','Lfdybxb65436(fvsdDDS')
-smtpObj.sendmail("justkiddingboat@gmail.com","michael.byrne@vice.com","go to bed!")
+smtpObj.sendmail("justkiddingboat@gmail.com","perekalskiy_igor@mail.ru","go to bed!")
 smtpObj.quit()
